35ec0f51bd6ddf6e0a469c344b01d1e2811e713c
SethCommichaux <yofi01@gmail.com>
2019-01-25 13:30:36 -0500

    Create extract_kaiju_reads.py

---
+++ genbank_protein/run_pipeline_scripts/extract_kaiju_reads.py
@@ -0,0 +1,21 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-k", help="kaiju output file")
+parser.add_argument("-s", help="query fastq file used by kaiju")
+parser.add_argument("-o", help="path name for output fasta file of extracted sequences")
+args = parser.parse_args()
+
+mapped_reads = {i.strip().split('\t')[1]:0 for i in open(args.k) if i[0] == "C"}
+
+print "Finished processing kaiju output file!!!"
+
+with open(args.o,'w') as out:
+	for h,i in enumerate(open(args.s)):
+		if h%4 == 0:
+			id = i[1:].split(' ')[0].strip()
+		elif h%4 == 1:
+			s = i.strip()
+			if id in mapped_reads:
+				out.write(">"+id+"\n"+s+"\n")
+
