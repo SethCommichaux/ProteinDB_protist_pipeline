db69ec6fff03a5d8a6c7b431184dd6739685851f
SethCommichaux <yofi01@gmail.com>
2019-02-03 10:55:12 -0500

    Update run_protist_pipeline.sh

--- genbank_protein/run_pipeline_scripts/run_protist_pipeline.sh
+++ genbank_protein/run_pipeline_scripts/run_protist_pipeline.sh
@@ -2,10 +2,9 @@
 #SBATCH --time=10:00:00
 #SBATCH --mem=50GB
 #SBATCH --cpus=12
-#SBATCH --qos=normal
+#SBATCH --qos=large
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --job-name="protist"
-
 
 # Load modules and software paths into environment
 #
@@ -15,32 +14,51 @@ NCBItaxonomy="/fs/cbcb-scratch/scommich/Protist/protist_db/"
 kaijuDB="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/binningDB.fasta.kaiju.fmi"
 diamondDB="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/queryDB"
 
+# Input and output directories
+# 
+input="/fs/cbcb-scratch/scommich/ProtistDB_protein/simulated_data/random10RefSeqGenomes/"
+output="/fs/cbcb-scratch/scommich/ProtistDB_protein/simulated_data/random10RefSeqGenomes/"
+
+mkdir $output
+
+################################################################
+################################################################
+################################################################
+
+for i in $input/*fastq
+
+do 
+
 # Fastq file(s) to be analyzed
 #
-reads_fastq="/fs/cbcb-scratch/scommich/ProtistDB_protein/simulated_data/random10RefSeqGenomes/10genomes.fastq"
+reads_fastq=$i
 
-# Output directory
+# Output file
 #
-output=${reads_fastq%.fastq}
-mkdir $output
+out=$output/$(basename ${i%.fastq})
+mkdir $out
 
 # Run kaiju to query fastq reads against protein sequence binning databse (binningDB.fasta)
 #
-$kaiju/kaiju -t $NCBItaxonomy/nodes.dmp -f $kaijuDB -i $reads_fastq -o $output/kaiju -z 12
-$kaiju/addTaxonNames -t $NCBItaxonomy/nodes.dmp -n $NCBItaxonomy/names.dmp -i $output/kaiju -o $output/kaiju.taxa -u -p
+$kaiju/kaiju -t $NCBItaxonomy/nodes.dmp -f $kaijuDB -i $reads_fastq -o $out/kaiju -z 12
+$kaiju/addTaxonNames -t $NCBItaxonomy/nodes.dmp -n $NCBItaxonomy/names.dmp -i $out/kaiju -o $out/kaiju.taxa -u -p
 
 # Extract reads that aligned to binning database
 #
-python extract_kaiju_reads.py -k $output/kaiju -s $reads_fastq -o $output/kaiju.fasta
+python extract_kaiju_reads.py -k $out/kaiju -s $reads_fastq -o $out/kaiju.fasta
+rm $out/kaiju
 
 # Align binned reads, with Diamond, to queryDB
 #
-time $diamond blastx --db $diamondDB --query $output/kaiju.fasta --threads 12 --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen --id 80 --subject-cover 50 --out $output/kaiju.fasta.diamond
+time $diamond blastx --db $diamondDB --query $out/kaiju.fasta --threads 12 --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen --id 60 --subject-cover 30 --out $out/kaiju.fasta.diamond
 
 # Process diamond output
 #
-python process_diamond_output.py -d $output/kaiju.fasta.diamond -f $output/kaiju.fasta -t /fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/genbank_map_ncbi_taxonomy.txt
+python subset_diamond_best_bitscore.py -d $out/kaiju.fasta.diamond -o $out/kaiju.fasta.diamond.subset
+python process_diamond_output.py -d $out/kaiju.fasta.diamond.subset -f $out/kaiju.fasta -t /fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/genbank_map_ncbi_taxonomy.txt -p /fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/proteinID_map_length.txt
 
 # Create files for producing sankey diagrams
 #
-python sankey_preprocess.py $output/kaiju.fasta.diamond.results
+python sankey_preprocess.py -n $NCBItaxonomy/nodes.dmp -f $NCBItaxonomy/fullnamelineage.dmp -t $out/kaiju.fasta.diamond.subset.results -o $out/
+
+done
