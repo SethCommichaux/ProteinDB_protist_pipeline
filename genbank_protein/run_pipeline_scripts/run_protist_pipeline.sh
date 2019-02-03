42f36819daa043850c4c1f1f4b2a81b24fa74cef
SethCommichaux <yofi01@gmail.com>
2019-02-03 11:13:20 -0500

    Update run_protist_pipeline.sh

--- genbank_protein/run_pipeline_scripts/run_protist_pipeline.sh
+++ genbank_protein/run_pipeline_scripts/run_protist_pipeline.sh
@@ -10,15 +10,20 @@
 #
 diamond="/fs/cbcb-scratch/scommich/ProtistDB_protein/diamond"
 kaiju="/fs/cbcb-scratch/scommich/ProtistDB_protein/kaiju/bin/" 
+createDB="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/createDB_scripts/"
+data="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/"
+run_pipeline="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/run_pipeline_scripts/"
 NCBItaxonomy="/fs/cbcb-scratch/scommich/Protist/protist_db/"
 kaijuDB="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/binningDB.fasta.kaiju.fmi"
 diamondDB="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/queryDB"
+
 
 # Input and output directories
 # 
 input="/fs/cbcb-scratch/scommich/ProtistDB_protein/simulated_data/random10RefSeqGenomes/"
 output="/fs/cbcb-scratch/scommich/ProtistDB_protein/simulated_data/random10RefSeqGenomes/"
-
+#input='/fs/cbcb-data/conserve/0-reads/2018_04_run/'
+#output='/fs/cbcb-scratch/scommich/CONSERVE/'
 mkdir $output
 
 ################################################################
@@ -40,13 +45,13 @@ mkdir $out
 
 # Run kaiju to query fastq reads against protein sequence binning databse (binningDB.fasta)
 #
-$kaiju/kaiju -t $NCBItaxonomy/nodes.dmp -f $kaijuDB -i $reads_fastq -o $out/kaiju -z 12
-$kaiju/addTaxonNames -t $NCBItaxonomy/nodes.dmp -n $NCBItaxonomy/names.dmp -i $out/kaiju -o $out/kaiju.taxa -u -p
+#$kaiju/kaiju -t $NCBItaxonomy/nodes.dmp -f $kaijuDB -i $reads_fastq -o $out/kaiju -z 12
+#$kaiju/addTaxonNames -t $NCBItaxonomy/nodes.dmp -n $NCBItaxonomy/names.dmp -i $out/kaiju -o $out/kaiju.taxa -u -p
 
 # Extract reads that aligned to binning database
 #
-python extract_kaiju_reads.py -k $out/kaiju -s $reads_fastq -o $out/kaiju.fasta
-rm $out/kaiju
+#python extract_kaiju_reads.py -k $out/kaiju -s $reads_fastq -o $out/kaiju.fasta
+#rm $out/kaiju
 
 # Align binned reads, with Diamond, to queryDB
 #
