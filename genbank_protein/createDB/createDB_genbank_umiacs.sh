3ba6249c30deffecd5109966427d27881f4df218
SethCommichaux <yofi01@gmail.com>
2019-02-05 16:53:30 -0500

    Update createDB_genbank_umiacs.sh

--- genbank_protein/createDB/createDB_genbank_umiacs.sh
+++ genbank_protein/createDB/createDB_genbank_umiacs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --time=200:00:00
-#SBATCH --mem=120GB
+#SBATCH --mem=50GB
 #SBATCH --cpus=12
 #SBATCH --qos=large
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -16,26 +16,30 @@ createDB="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/createDB_s
 data="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/data/"
 run_pipeline="/fs/cbcb-scratch/scommich/ProtistDB_protein/genbank_protein/run_pipeline_scripts/"
 
+
+# Change to data directory
+#
+cd $data
+
+
 # Download, decompress and concatenate genbank protein sequence files
 #
 wget ftp://ftp.ncbi.nih.gov/ncbi-asn1/protein_fasta/gb*.fsa_aa.gz
 gunzip *gz
 cat *.fsa_aa > genbank.pep
+rm *fsa_aa
+
 
 # Download, decompress NCBI taxonomy files
 #
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz
 tar xvf new_taxdump.tar.gz
+rm new_taxdump.tar.gz rankedlineage.dmp taxidlineage.dmp type* citations.dmp delnodes.dmp division.dmp gencode.dmp host.dmp merged.dmp
 
-
-# Create mapping file that matches genbank fasta sequence IDs to NCBI taxonomy
-#
-python map_gbkID_2_taxonomy.py -g queryDB.fasta -t ../../Protist/protist_db/fullnamelineage.dmp -o genbank_map_ncbi_taxonomy.txt
-python protein_length_map.py -f queryDB.fasta -o proteinID_map_length.txt
 
 # Extract protist protein sequences from genbank protein sequence file 
 #
-time python filter_genbank_for_protist_taxa.py -g genbank.pep -l /fs/cbcb-scratch/scommich/Protist/protist_db/fullnamelineage.dmp -o genbank_protists.pep
+time python $createDB/filter_genbank_for_protist_taxa.py -g genbank.pep -l fullnamelineage.dmp -o genbank_protists.pep
 rm genbank.pep
 
 
@@ -45,19 +49,25 @@ $diamond makedb --in genbank_protists.pep --db genbank_protists --threads 12
 $diamond blastp --db genbank_protists --query genbank.pepwithout_protists --threads 12 --outfmt 6 --id 70 --query-cover 30 --max-target-seqs 1 --out eukaryote2bacteria.txt
 
 
-# Create binning database
-#
-python reformat_binningDB.py -d ../data/genbank_protists.pep -m ../data/genbank_map_ncbi_taxonomy.txt -o ../data/binningDB.fasta
-$kaiju/mkbwt -o ../data/binningDB.fasta.kaiju -n 12 -l 100000 ../data/binningDB.fasta
-$kaiju/mkfmi ../data/binningDB.fasta.kaiju
-rm ../data/binningDB.fasta.kaiju.bwt ../data/binningDB.fasta.kaiju.sa
-
-
 # Add non-protist protein homologs to protist proteins. Then create querying database
 #
-python extract_fasta_diamond.py -d eukaryote2bacteria.txt -s genbank.pepwithout_protists -o nonprotists.pep
+python $createDB/extract_fasta_diamond.py -d eukaryote2bacteria.txt -s genbank.pepwithout_protists -o nonprotists.pep
 cat nonprotists.pep genbank_protists.pep > queryDB.fasta
 $diamond makedb --in queryDB.fasta --db queryDB --threads 12
+
+
+# Create mapping file that matches genbank fasta sequence IDs to NCBI taxonomy
+#
+python $createDB/map_gbkID_2_taxonomy.py -g queryDB.fasta -t fullnamelineage.dmp -o genbank_map_ncbi_taxonomy.txt
+python $createDB/protein_length_map.py -f queryDB.fasta -o proteinID_map_length.txt
+
+
+# Create binning database
+#
+python $createDB/reformat_binningDB.py -d genbank_protists.pep -m genbank_map_ncbi_taxonomy.txt -o binningDB.fasta
+$kaiju/mkbwt -o binningDB.fasta.kaiju -n 12 -l 100000 binningDB.fasta
+$kaiju/mkfmi binningDB.fasta.kaiju
+rm binningDB.fasta.kaiju.bwt binningDB.fasta.kaiju.sa
 
 
 # Download, decompress and process protist BUSCO genes
@@ -73,3 +83,8 @@ cat alveolata_stramenophiles_ensembl/ancestral* protists_ensembl/ancestral* > pr
 #
 $diamond makedb --in protist_ancestral_busco_proteins.fasta --db protist_ancestral_busco_proteins --threads 12
 $diamond blastp --db protist_ancestral_busco_proteins --query genbank_protists.pep --threads 12 --outfmt 6 --id 50 --subject-cover 50 --out protist_busco_candidates.txt --max-target-seqs 1
+
+
+# Clean-up data directory
+#
+rm -r genbank.pep alveolata_stramenophiles_ensembl* protists_ensembl* eukaryote2bacteria.txt genbank.pepwithout_protists genbank_protists.dmnd genbank_protists.pep
