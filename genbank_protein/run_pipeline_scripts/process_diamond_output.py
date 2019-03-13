0a6c229dfb40c5ee4cfc41e9f0896a4f6b19d142
SethCommichaux <yofi01@gmail.com>
2019-03-13 11:57:07 -0400

    Update process_diamond_output.py

--- genbank_protein/run_pipeline_scripts/process_diamond_output.py
+++ genbank_protein/run_pipeline_scripts/process_diamond_output.py
@@ -1,140 +1,152 @@
 import argparse
-from collections import Counter
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", type=str,
-						help="Diamond output file (--outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen)")
+                                                help="Diamond output file (--outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen)")
 parser.add_argument("-t", type=str,
-						help="The genbank_map_ncbi_taxonomy.txt file from creating database.")
+                                                help="The genbank_map_ncbi_taxonomy.txt file from creating database.")
 parser.add_argument("-f", type=int,
-			help="Read count of input file")
+                                                help="Read count of input file")
+parser.add_argument("-ANI", type=float,
+                                                help="Minimum average nucleotide identity (e.g. 80 for 80%) for reads aligned to phylogroup")
 parser.add_argument("-num_reads", type=str,
-						help="Minimum number of mapped reads to include phylogroup")
+                                                help="Minimum number of mapped reads to include phylogroup")
 parser.add_argument("-num_prots", type=str,
-						help="Minimum number of mapped proteins to include phylogroup")
+                                                help="Minimum number of mapped proteins to include phylogroup")
 args = parser.parse_args()
 
-print "Parsed args!!!"
 
 def build_taxa_dicts():
-	fullnamelineage = {i.strip().split('\t')[1]:0 for i in open(args.d)}
-	for i in open(args.t):
-		tmp = i.strip().split('\t')
-		gb_id = tmp[0]
-		prot_len = int(tmp[5])
-		lineage = tmp[3]
-		fullnamelineage[gb_id] = [prot_len,lineage]
-	print "Built taxa dictionary!!!"
-	return fullnamelineage
+        fullnamelineage = {}
+        proteins = {i.strip().split('\t')[1]:0 for i in open(args.d)}
+        for i in open(args.t):
+                tmp = i.strip().split('\t')
+                gb_id = tmp[0]
+                if gb_id in proteins:
+                        prot_len = int(tmp[5])
+                        lineage = tmp[3]
+                        fullnamelineage[gb_id] = [prot_len,lineage]
+        print "Built taxa dictionary!!!"
+        return fullnamelineage
 
 
-def LCA(x,y):
-	new_lineage = ''
-	x=map(lambda z: z.strip(), x.split(';'))
-	y=map(lambda z: z.strip(), y.split(';'))
-	for i in range(min(len(x),len(y))):
-		if x[i] == y[i]:
-			new_lineage += x[i]+';'
-		else:
-			return new_lineage
+def LCA(taxa):
+        if len(taxa) == 1:
+                return taxa[0]
+        for i,j in enumerate(taxa):
+                if 'EUKARYOTA' not in j:
+                        return False
+                else:
+                        c = 0
+                        j = map(lambda z: z.strip(), j.split(';'))
+                        if i == 0:
+                                new_lineage = j
+                        else:
+                                for i in range(min(len(j),len(new_lineage))):
+                                        if j[i] == new_lineage[i]:
+                                                c += 1
+                                new_lineage = new_lineage[:c]
+        new_lineage = ';'.join(new_lineage)+';'
+        return new_lineage
+
+
+def calc_ANI(pident,aln):
+        pident = map(lambda x:x/100.0,pident)
+        total_len = sum(aln)
+        weight = 0
+        for i in range(len(pident)):
+                weight += pident[i]*aln[i]
+        ANI = weight/float(total_len)
+        if ANI < args.ANI/100.0: return False
+        return weight/total_len
 
 
 fullnamelineage = build_taxa_dicts()
-read_LCA = {}
-species_protein_count = {}
-ANI = {}
-ANI_lineage = {}
-ANI_cumulative = {}
-species_protein_count_cumulative = {}
-read_LCA_cumulative = {}
+first_pass = {}
 
 
+# collect values in file
 # qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen
 for i in open(args.d):
-	tmp = i.strip().split('\t')
-	read,gb_id,pident,qlen,start,stop,bitscore = tmp[0],tmp[1],float(tmp[2]),int(tmp[-1]),int(tmp[6]),int(tmp[7]),float(tmp[11])
-	lineage = fullnamelineage[gb_id][1]
-	if 'BACTERIA' in lineage: 
-		read_LCA[read] = None
-		continue
- 	if 'VIRUS' in lineage: 
-		read_LCA[read] = None
-		continue
-        if 'ARCHAEA' in lineage:
-                read_LCA[read] = None
+        tmp = i.strip().split('\t')
+        read,gb_id,pident,aln = tmp[0],tmp[1],float(tmp[2]),float(tmp[3])
+        lineage = fullnamelineage[gb_id][1]
+        if read not in first_pass:
+                first_pass[read] = [[lineage],[gb_id],[pident],[aln]]
+        else:
+                first_pass[read][0].append(lineage)
+                first_pass[read][1].append(gb_id)
+                first_pass[read][2].append(pident)
+                first_pass[read][3].append(aln)
+
+
+# calculate LCA for each read
+for read,v in first_pass.items():
+        taxa = list(set(v[0]))
+        read_LCA = LCA(taxa)
+        if read_LCA == False:
+                del first_pass[read]
                 continue
-	if read not in read_LCA:
-		read_LCA[read] = lineage
-	else:
-		if read_LCA[read] == None: continue
-		read_LCA[read] = LCA(read_LCA[read],lineage)
-	if lineage not in species_protein_count:
-		species_protein_count[lineage] = [gb_id]
-	else:
-		if gb_id not in species_protein_count[lineage]:
-			species_protein_count[lineage].append(gb_id)
-	if read not in ANI: ANI[read] = [pident]
-	else: ANI[read].append(pident)
-
-print "Processed diamond file!!!"
+        else: first_pass[read][0] = read_LCA
 
 
-for read,pident in ANI.items():
-	if read in read_LCA:
-		if read_LCA[read] in ANI_lineage:
-			ANI_lineage[read_LCA[read]] += pident
-		else:
-			ANI_lineage[read_LCA[read]] = pident
+# group results by phylogroup
+results = {}
 
-ANI = {}
-
-for lineage,pident in ANI_lineage.items():
-	if lineage == None: continue
-	tmp = map(lambda z: z.strip(),lineage.split(';'))
-	for rank in range(1,len(tmp)):
-		line = ';'.join(tmp[0:rank])+';'
-		if line in ANI_cumulative:
-			ANI_cumulative[line] += pident
-		else:
-			ANI_cumulative[line] = pident
-
-ANI_lineage = {}
-
-for species,protein_count in species_protein_count.items():
-	tmp = map(lambda z: z.strip(),species.split(';'))
-	for rank in range(1,len(tmp)):
-		line = ';'.join(tmp[0:rank])+';'
-		if line in species_protein_count_cumulative:
-			species_protein_count_cumulative[line] = species_protein_count_cumulative[line] | set(protein_count)
-		else:
-			species_protein_count_cumulative[line] = set(protein_count)
+for read,v in first_pass.items():
+        LCA = v[0]
+        proteins = v[1]
+        pident = v[2]
+        aln = v[3]
+        if LCA not in results:
+                results[LCA] = [1,proteins,pident,aln]
+        else:
+                results[LCA][0] += 1 # read_count
+                results[LCA][1] += proteins
+                results[LCA][2] += pident
+                results[LCA][3] += aln
 
 
-LCA_readcount = Counter(read_LCA.values())
-species_protein_count,read_LCA = {},{}
+# calculate ANI for phylogroup
+first_pass = {}
+
+for LCA,v in results.items():
+        read_count = v[0]
+        protein_count = set(v[1])
+        pident = v[2]
+        aln = v[3]
+        ANI = calc_ANI(pident,aln)
+        if ANI == False:
+                del results[LCA]
+                continue
+        results[LCA] = [read_count,protein_count,ANI,pident,aln]
 
 
-for LCA,read_count in LCA_readcount.items():
-	if LCA == None: continue
-	tmp = map(lambda z: z.strip(),LCA.split(';'))
-	for rank in range(1,len(tmp)):
-		line = ';'.join(tmp[0:rank])+';'
-		if line in read_LCA_cumulative:
-			read_LCA_cumulative[line] += read_count
-		else:
-			read_LCA_cumulative[line] = read_count
+# retrieve cumulative results for each phylogroup
+cumulative_results = {}
 
-print "Proccessed dictionaries!!!"
-LCA_readcount = {}
+for lineage,v in results.items():
+        tmp = map(lambda z: z.strip(),lineage.split(';'))
+        for rank in range(1,len(tmp)):
+                line = ';'.join(tmp[0:rank])+';'
+                if line not in cumulative_results:
+                        cumulative_results[line] = v
+                else:
+                        cumulative_results[line][0] += v[0]
+                        cumulative_results[line][1] = cumulative_results[line][1] | v[1]
+                        pident += cumulative_results[line][3]
+                        aln += cumulative_results[line][4]
+                        cumulative_results[line][2] = calc_ANI(pident,aln)
 
-with open(args.d+'.results','w') as out:
-	out.write('Phylogroup\tRead_count\tRel_abund\tProtein_count\tProt_Seq_Len\tANI\n')
-	for k,v in sorted(read_LCA_cumulative.items(),key=lambda x: x[1]):
-		if k == None: continue
-		if v < int(args.num_reads): continue
-		if len(species_protein_count_cumulative[k]) < int(args.num_prots): continue
-		if 'EUKARYOTA' in k:
-			if 'METAZOA' not in k:
-				if 'FUNGI;' not in k:
-					if 'EMBRYOPHYTA' not in k:
-						out.write(str(k)+'\t'+str(v)+'\t'+str(v/float(args.f))+'\t'+str(len(species_protein_count_cumulative[k]))+'\t'+str(sum(map(lambda z: fullnamelineage[z][0],species_protein_count_cumulative[k])))+'\t'+str(float(sum(ANI_cumulative.get(k,1)))/len(ANI_cumulative.get(k,1)))+'\n')
+
+with open(args.d+'.results','w') as out, open(args.d+'.functions','w') as out2:
+        out.write('Phylogroup\tRead_count\tRel_abund\tProtein_count\tProt_Seq_Len\tANI\n')
+        for k,v in sorted(cumulative_results.items()):
+                if v[0] < int(args.num_reads): continue
+                if len(v[1]) < int(args.num_prots): continue
+                if 'EUKARYOTA' in k:
+                        if 'METAZOA' not in k:
+                                if 'FUNGI;' not in k:
+                                        if 'EMBRYOPHYTA' not in k:
+                                                out.write(str(k)+'\t'+str(v[0])+'\t'+str(v[0]/float(args.f))+'\t'+str(len(v[1]))+'\t'+str(sum(map(lambda z: fullnamelineage[z][0],v[1])))+'\t'+str(v[2])+'\n')
+                                                out2.write(str(k)+'\t'+str(v[1])+'\n')
